0a855249c2a4c9fc599cb708591e6b7a7673437e
Noah Napitupulu <noah250103@gmail.com>
2022-06-05 09:59:00 +0700

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_application_tugas_kelompok_nftmarket/home.dart';
+import 'package:flutter_application_tugas_kelompok_nftmarket/Dashboard.dart';
 import 'package:flutter_application_tugas_kelompok_nftmarket/sign_in.dart';
 
 import 'sign_up.dart';
